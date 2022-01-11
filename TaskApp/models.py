de33ed7c8aa730364ba52ad4444bf15f91546853
Flamcom27 <27flamcom06@gmail.com>
2022-01-11 14:29:36 +0300

    TaskModel is created, templates home and tasks are created, views Home and Tasks are created

--- TaskApp/models.py
+++ TaskApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class TaskModel(models.Model):
+    title = models.CharField(max_length=30, verbose_name="Task", editable=True)
+    description = models.CharField(max_length=250, null=True, blank=True, editable=True)
+    created = models.DateTimeField()
+    deadline = models.DateField(null=True, blank=True)
+    done = models.BooleanField(default=False, editable=True)
+
+    def __repr__(self):
+        return self.title
--- TaskApp/views.py
+++ TaskApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import TaskModel
+
 
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def task_view(request):
+    tasks = TaskModel.objects.all()
+    return render(request, 'tasks.html', {'tasks': tasks})
